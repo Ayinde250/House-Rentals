a1923fadeb6c3795399e3c5769f8836895e75487
Ayinde250 <ayindeadekunle34@gmail.com>
2024-09-15 22:53:47 +0100

    media query sm

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 const navMeun = document.querySelector(".menu_bar");
-const navLinks = document.querySelector("nav_link");
+const navLinks = document.querySelector(".nav_link");
 
 navMeun.addEventListener('click', () => {
 
    navLinks.classList.toggle("active");
-   console.log("click");
+   
 });
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const navMenu = document.querySelector(".menu_bar");
-//     const navLinks = document.querySelector(".nav_link");
-
-//     if (navMenu && navLinks) {
-//         navMenu.addEventListener('click', () => {
-//             navLinks.classList.toggle("active");
-//             console.log("click");
-//         });
-//     } else {
-//         console.error("Menu bar or nav links not found");
-//     }
-// });
 
